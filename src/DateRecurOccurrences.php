60f91ba9a354289d2f5076f437f3cbb1ad1c6d16
dpi <pro@danielph.in>
2021-11-20 03:33:12 +0800

    missing the thing.

--- src/DateRecurOccurrences.php
+++ src/DateRecurOccurrences.php
@@ -265,7 +265,7 @@ class DateRecurOccurrences implements EventSubscriberInterface, EntityTypeListen
    */
   protected function fieldStorageDelete(FieldStorageDefinitionInterface $fieldDefinition): void {
     $tableName = static::getOccurrenceCacheStorageTableName($fieldDefinition);
-    if ($this->database->schema()->tableExists($tableName)) {
+    if (!$this->database->schema()->tableExists($tableName)) {
       return;
     }
 
