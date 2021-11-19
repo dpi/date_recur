a3dfa4e89f2d7e175efd6f17c21d6a3d98a9aebc
dpi <pro@danielph.in>
2021-11-20 02:38:58 +0800

    dpi changes

--- tests/src/Kernel/DateRecurViewsOccurrenceFilterAttachedTest.php
+++ tests/src/Kernel/DateRecurViewsOccurrenceFilterAttachedTest.php
@@ -25,7 +25,7 @@ class DateRecurViewsOccurrenceFilterAttachedTest extends DateRecurViewsOccurrenc
   /**
    * {@inheritdoc}
    */
-  protected function setUp($import_test_views = TRUE) : void {
+  protected function setUp($import_test_views = TRUE): void {
     parent::setUp(FALSE);
 
     // This is the name of the attached field.
